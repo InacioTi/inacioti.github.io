9c246db28cb1ba99cf0539bd2ce6093fa6d12bad
inacioti <jose.inaciot@outlook.com>
2023-02-09 10:19:43 -0300

    UpdateALL

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230208.190610';
+const cacheName = 'chirpy-20230209.101754';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
